6f01a7e0f1e858a2dcbb834219e05e761c3a3af0
zengcheng0820 <zengcheng0820@gmail.com>
2017-09-06 20:13:28 -0400

    edit the person3 file

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,6 +43,7 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+		String reverse = new StringBuffer(input).reverse().toString();
+	  return reverse;
 	}
 }
