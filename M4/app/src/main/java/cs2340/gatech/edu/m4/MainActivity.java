d8f33025f5ed225db7d01d1e41f0cd1729fc84d8
xli684 <lixianelite@gatech.edu>
2017-09-23 15:10:56 -0400

    M4 file created

---
+++ M4/app/src/main/java/cs2340/gatech/edu/m4/MainActivity.java
@@ -0,0 +1,26 @@
+package cs2340.gatech.edu.m4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logoutIntent = new Intent (MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(logoutIntent);
+                finish();
+            }
+        });
+    }
+}
