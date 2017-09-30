edc259aeffe1039f872df0904e83dabaa1010ed4
tnan3 <tnan3@gatech.edu>
2017-09-30 10:22:06 -0400

    project

---
+++ 2340project/app/src/main/java/cs2340/gatech/edu/a2340project/activity/WelcomeActivity.java
@@ -0,0 +1,68 @@
+package cs2340.gatech.edu.a2340project.activity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cs2340.gatech.edu.a2340project.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button loginButton2 = (Button) findViewById(R.id.login_button2);
+        loginButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent (WelcomeActivity.this, LoginActivity.class);
+                WelcomeActivity.this.startActivity(loginIntent);
+                finish();
+            }
+        });
+
+        Button registerButton2 = (Button) findViewById(R.id.register_button2);
+        registerButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent (WelcomeActivity.this, RegisterActivity.class);
+                WelcomeActivity.this.startActivity(registerIntent);
+                finish();
+            }
+        });
+
+        Button ExitButton = (Button) findViewById((R.id.exit_button));
+        ExitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /*public void onClick(View view) {
+                finish();
+            }*/
+            public void onClick(View view){
+                AlertDialog.Builder alertbuilder = new AlertDialog.Builder(WelcomeActivity.this);
+                alertbuilder.setMessage("Do you want to exit?");
+                alertbuilder.setCancelable(true);
+                alertbuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                alertbuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alert = alertbuilder.create();
+                alert.show();
+            }
+        });
+    }
+}
+
---
+++ 2340project/app/src/main/java/cs2340/gatech/edu/a2340project/model/User.java
@@ -0,0 +1,49 @@
+package cs2340.gatech.edu.a2340project.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by tianyunan on 9/30/17.
+ */
+
+public class User {
+    private int id;
+    public static List<String> UserType = Arrays.asList("User", "Admin");
+    private String username, password, email, usertype;
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getUsername(){
+        return username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setUsertype(String usertype){
+        this.usertype = usertype;
+    }
+    public String getUsertype(){
+        return usertype;
+    }
+}
