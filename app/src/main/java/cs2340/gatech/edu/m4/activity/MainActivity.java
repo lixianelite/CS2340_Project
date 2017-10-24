6c99a037290520c5885fab283612240333cb7256
xli684 <lixianelite@gatech.edu>
2017-10-24 01:22:02 -0400

    mainActivity changed

--- app/src/main/java/cs2340/gatech/edu/m4/activity/MainActivity.java
+++ app/src/main/java/cs2340/gatech/edu/m4/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        /*Button logoutButton = (Button) findViewById(R.id.logout_button);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,8 +55,36 @@ public class MainActivity extends AppCompatActivity {
                 Intent ReportIntent = new Intent(MainActivity.this, ReportActivity.class);
                 MainActivity.this.startActivity(ReportIntent);
             }
-        });
+        });*/
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.report_button:
+                Intent ReportIntent = new Intent(MainActivity.this, ReportActivity.class);
+                MainActivity.this.startActivity(ReportIntent);
+                break;
+            case R.id.loaddata_button:
+                readSDFile();
+                Intent DataIntent = new Intent(MainActivity.this, DataListActivity.class);
+                MainActivity.this.startActivity(DataIntent);
+                break;
+            case R.id.logout_button:
+                Intent logoutIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+                MainActivity.this.startActivity(logoutIntent);
+                finish();
+                break;
+            default:
+        }
+        return true;
     }
 
     private void readSDFile() {
