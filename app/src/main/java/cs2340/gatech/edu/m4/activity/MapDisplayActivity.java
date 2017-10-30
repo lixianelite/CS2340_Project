14b7d47fc9ff326f7b3d4383314b08e4ec8b9e2a
xli684 <lixianelite@gatech.edu>
2017-10-30 03:29:03 -0400

    map activity added

--- app/src/main/java/cs2340/gatech/edu/m4/activity/MapDisplayActivity.java
+++ app/src/main/java/cs2340/gatech/edu/m4/activity/MapDisplayActivity.java
@@ -1,10 +1,13 @@
 package cs2340.gatech.edu.m4.activity;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,38 +16,38 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.sql.ClientInfoStatus;
+
 import cs2340.gatech.edu.m4.*;
 
 /**
  * Created by bravado on 10/29/17.
  */
 
-public class MapDisplayActivity extends AppCompatActivity implements
-        OnMapReadyCallback{
+public class MapDisplayActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_map_activity);
 
-        setContentView(R.layout.google_map);
-
-        Log.d("GoogMapDisplay", "Activity started");
-
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_display);
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
