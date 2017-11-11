a54a6d7c15e3fb49912c4a21b3a8a4bb2805b2f6
xli684 <lixianelite@gatech.edu>
2017-11-10 23:46:44 -0500

    Minor change

--- app/src/main/java/cs2340/gatech/edu/m4/activity/ClusterMapActivity.java
+++ app/src/main/java/cs2340/gatech/edu/m4/activity/ClusterMapActivity.java
@@ -11,10 +11,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
-import com.google.maps.android.ui.IconGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
